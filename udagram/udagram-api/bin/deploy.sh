fcc58cb7fc06b989b1f6a6b99322bd764ff697b3
KiritoX <nadersayed001@gmail.com>
2023-01-05 16:27:03 +0200

    deploying

---
+++ udagram/udagram-api/bin/deploy.sh
@@ -0,0 +1,4 @@
+eb init udagram --region us-east-1 --platform node.js
+eb create --sample udagram-env
+eb use udagram-env
+eb setenv POSTGRES_USERNAME=postgres POSTGRES_PASSWORD=postgres POSTGRES_DB=udagram POSTGRES_HOST=database-1.cjetexzbx0hp.us-east-1.rds.amazonaws.com AWS_REGION=us-east-1 AWS_BUCKET=myfrontbackfwd AWS_PROFILE=default URL=http://udagram-env.eba-pxh4pcfa.us-east-1.elasticbeanstalk.com/
--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: true,
-  appName: "Udagram",
-  apiHost: "http://localhost:8080/api/v0",
+  appName: "udagram",
+  apiHost: "http://udagram-env.eba-pxh4pcfa.us-east-1.elasticbeanstalk.com/api/v0",
 };
 
 /*
