0d391f6fc0b3f0553bc83cd0d0faefa3eac8c27b
KiritoX <nadersayed001@gmail.com>
2023-01-06 20:40:23 +0200

    [UPDATE] enviromt.ts

--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   appName: 'udagram',
-  apiHost: 'udagram-env.eba-ybyw3gv9.us-east-1.elasticbeanstalk.com/api/v0',
+  apiHost: 'http://udagram-env.eba-ybyw3gv9.us-east-1.elasticbeanstalk.com/api/v0',
 };
 
 /*
